fbab9ae03a2638b9696b252dfae8ca4ac2d9d80b
Umer Majeed <umarmajeed786k@gmail.com>
2026-08-02 03:03:22 +0500

    configure new UI for the dashboard

--- NexZeus/obj/Debug/net10.0-windows/NexZeus.AssemblyInfo.cs
+++ NexZeus/obj/Debug/net10.0-windows/NexZeus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexZeus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d77dc13f928e6d1aa17ffb66e0d1763ac2c096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4098fb703d7356119cf72602108adbcd667a7c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexZeus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexZeus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
